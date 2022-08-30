ddb3462d8d62657e02dd325b8f2a28e27db73439
Jason Carrete <jasoncarrete5@gmail.com>
2022-08-30 12:19:56 -0400

    Implement read/write in I2CDev

--- flight-controller/driver/i2c-dev.cpp
+++ flight-controller/driver/i2c-dev.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cassert>
 #include <cstdint>
 #include <string>
@@ -24,6 +25,11 @@ I2CDev::I2CDev(const std::string& i2c_bus, uint16_t address)
     if ((funcs & I2C_FUNC_I2C) == 0) {
         throw I2CFunctionNotSupported{"I2C_FUNC_I2C"};
     }
+
+    // Need NOSTART functionality to avoid a repeated start during write
+    if ((funcs & I2C_FUNC_NOSTART) == 0) {
+        throw I2CFunctionNotSupported{"I2C_FUNC_NOSTART"};
+    }
 }
 
 I2CDev::I2CDev(const I2CDev& other)
@@ -44,19 +50,45 @@ I2CDev::~I2CDev()
     // TODO Log if close fails
 }
 
+std::uint8_t I2CDev::read(std::uint8_t reg) const
+{
+    std::uint8_t result;
+    std::array<i2c_msg, 2> msgs{
+            i2c_msg{
+                    .addr = address,
+                    .flags = 0,
+                    .len = 1,
+                    .buf = &reg,
+            },
+            i2c_msg{
+                    .addr = address,
+                    .flags = I2C_M_RD,
+                    .len = 1,
+                    .buf = &result,
+            }
+    };
+
+    i2c_rdwr_ioctl_data data{
+            .msgs = msgs.data(),
+            .nmsgs = msgs.size(),
+    };
+    check_syscall(ioctl(bus_fd, I2C_RDWR, &data));
+
+    return result;
+}
+
 std::vector<std::uint8_t> I2CDev::read(std::uint8_t start_reg, std::uint16_t length) const
 {
     assert(length > 0);  // Cannot read 0 bytes
 
     std::vector<std::uint8_t> result(length);
-    i2c_msg msgs[] = {
-            {
+    std::array<i2c_msg, 2> msgs{
+            i2c_msg{
                     .addr = address,
-                    .flags = 0,
                     .len = 1,
                     .buf = &start_reg,
             },
-            {
+            i2c_msg{
                     .addr = address,
                     .flags = I2C_M_RD,
                     .len = length,
@@ -65,11 +97,56 @@ std::vector<std::uint8_t> I2CDev::read(std::uint8_t start_reg, std::uint16_t len
     };
 
     i2c_rdwr_ioctl_data data{
-            .msgs = msgs,
-            .nmsgs = 2,
+            .msgs = msgs.data(),
+            .nmsgs = msgs.size(),
     };
-
     check_syscall(ioctl(bus_fd, I2C_RDWR, &data));
 
     return result;
+}
+
+void I2CDev::write(std::uint8_t reg, std::uint8_t byte) const
+{
+    std::array<i2c_msg, 2> msgs = {
+            i2c_msg{
+                    .addr = address,
+                    .len = 1,
+                    .buf = &reg
+            },
+            i2c_msg{
+                    .addr = address,
+                    .flags = I2C_M_NOSTART,
+                    .len = 1,
+                    .buf = &byte
+            },
+    };
+
+    i2c_rdwr_ioctl_data data{
+            .msgs = msgs.data(),
+            .nmsgs = msgs.size(),
+    };
+    check_syscall(ioctl(bus_fd, I2C_RDWR, &data));
+}
+
+void I2CDev::write(std::uint8_t reg, const std::vector<std::uint8_t>& buffer) const
+{
+    std::array<i2c_msg, 2> msgs = {
+            i2c_msg{
+                    .addr = address,
+                    .len = 1,
+                    .buf = &reg
+            },
+            i2c_msg{
+                    .addr = address,
+                    .flags = I2C_M_NOSTART,
+                    .len = static_cast<std::uint16_t>(buffer.size() + 1),
+                    .buf = const_cast<std::uint8_t*>(buffer.data())
+            },
+    };
+
+    i2c_rdwr_ioctl_data data{
+            .msgs = msgs.data(),
+            .nmsgs = msgs.size(),
+    };
+    check_syscall(ioctl(bus_fd, I2C_RDWR, &data));
 }
