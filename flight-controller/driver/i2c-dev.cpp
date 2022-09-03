0b728793cf73c64b111187e1e2bb390fd411316a
Jason Carrete <jasoncarrete5@gmail.com>
2022-09-03 16:24:10 -0400

    Add documentation to i2c-dev

--- flight-controller/driver/i2c-dev.cpp
+++ flight-controller/driver/i2c-dev.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <cassert>
 #include <cstdint>
+#include <limits>
 #include <string>
 #include <stdexcept>
 #include <vector>
@@ -14,7 +15,13 @@
 #include "i2c-dev.h"
 #include "../error.h"
 
-I2CDev::I2CDev(const std::string& i2c_bus, uint16_t address)
+/**
+ * @brief Construct a I2C device.
+ *
+ * @param i2c_bus Path to an I2C device file.
+ * @param address Device I2C address to communicate with.
+ */
+I2CDev::I2CDev(const std::string& i2c_bus, std::uint16_t address)
     : bus_fd{open(i2c_bus.c_str(), O_RDWR)}, address{address}
 {
     check_syscall(bus_fd);
@@ -51,6 +58,12 @@ I2CDev::~I2CDev()
     // TODO Log if close fails
 }
 
+/**
+ * @brief Read a byte from a device register.
+ *
+ * @param reg Device register to read from.
+ * @return The byte read from the device register.
+ */
 std::uint8_t I2CDev::read(std::uint8_t reg) const
 {
     std::uint8_t result;
@@ -78,6 +91,15 @@ std::uint8_t I2CDev::read(std::uint8_t reg) const
     return result;
 }
 
+/**
+ * @brief Read consecutive bytes starting from a register.
+ *
+ * This function will fail when trying to read 0 bytes.
+ *
+ * @param start_reg Device register to start reading bytes from.
+ * @param length Number of consecutive bytes to read.
+ * @return A vector containing the bytes read.
+ */
 std::vector<std::uint8_t> I2CDev::read(std::uint8_t start_reg, std::uint16_t length) const
 {
     assert(length > 0);  // Cannot read 0 bytes
@@ -107,6 +129,12 @@ std::vector<std::uint8_t> I2CDev::read(std::uint8_t start_reg, std::uint16_t len
     return result;
 }
 
+/**
+ * @brief Write a byte to a device register.
+ *
+ * @param reg Device register to write a byte into.
+ * @param byte Byte to write into the device register.
+ */
 void I2CDev::write(std::uint8_t reg, std::uint8_t byte) const
 {
     std::array<i2c_msg, 2> msgs = {
@@ -131,8 +159,32 @@ void I2CDev::write(std::uint8_t reg, std::uint8_t byte) const
     check_syscall(ioctl(bus_fd, I2C_RDWR, &data));
 }
 
+/**
+ * @brief Write a buffer of bytes to consecutive registers.
+ *
+ * The buffer must not be empty and it's size must fit within a std::uint16_t.
+ *
+ * Each byte from buffer is written to consecutive registers on the device
+ * starting with the specified register.
+ *
+ * Example:
+ * write(24, std::vector<std::uint8_t>{1, 2, 3, 4})
+ *
+ * Register 24 => 1
+ * Register 25 => 2
+ * Register 26 => 3
+ * Register 27 => 4
+ *
+ * @param reg Device register to start writing data into.
+ * @param buffer Buffer containing bytes to write.
+ */
 void I2CDev::write(std::uint8_t reg, const std::vector<std::uint8_t>& buffer) const
 {
+    // buffer must not be empty
+    assert(!buffer.empty());
+    // buffer must not exceed std::uint16_t maximum to be used as a length in an i2c_msg
+    assert(buffer.size() <= std::numeric_limits<std::uint16_t>::max());
+
     std::array<i2c_msg, 2> msgs = {
         i2c_msg{
             .addr = address,
@@ -143,8 +195,8 @@ void I2CDev::write(std::uint8_t reg, const std::vector<std::uint8_t>& buffer) co
         i2c_msg{
             .addr = address,
             .flags = I2C_M_NOSTART,
-            .len = static_cast<std::uint16_t>(buffer.size() + 1),
-            .buf = const_cast<std::uint8_t*>(buffer.data())
+            .len = static_cast<std::uint16_t>(buffer.size()),
+            .buf = const_cast<std::uint8_t*>(buffer.data())  // The data is copied in the kernel so this is safe
         },
     };
 
