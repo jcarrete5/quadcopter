bbc51bce0f1d1f50675ce6ed4297cc4d699c11ff
Jason Carrete <jasoncarrete5@gmail.com>
2022-09-04 19:17:20 -0400

    Make I2CDev thread-safe

--- flight-controller/driver/i2c-dev.h
+++ flight-controller/driver/i2c-dev.h
@@ -2,7 +2,10 @@
 #define FLIGHT_CONTROLLER_I2C_DEV_H
 
 #include <cstdint>
+#include <map>
+#include <mutex>
 #include <string>
+#include <tuple>
 #include <vector>
 
 /**
@@ -14,17 +17,10 @@ class I2CDev {
 public:
     explicit I2CDev(const std::string& i2c_bus, uint16_t address);
     ~I2CDev();
-    I2CDev(const I2CDev& other);
+    I2CDev(const I2CDev& other) = delete;
+    I2CDev& operator=(const I2CDev& other) = delete;
     I2CDev(I2CDev&& other) = default;
-    I2CDev& operator=(I2CDev other);
     I2CDev& operator=(I2CDev&& other) = default;
-
-    friend void swap(I2CDev& first, I2CDev& second)
-    {
-        using std::swap;
-        swap(first.bus_fd, second.bus_fd);
-        swap(first.address, second.address);
-    }
 
     [[nodiscard]] std::uint8_t read(std::uint8_t reg) const;
     [[nodiscard]] std::vector<std::uint8_t> read(std::uint8_t start_reg,
@@ -33,8 +29,13 @@ public:
     void write(std::uint8_t reg, std::vector<uint8_t> buffer) const;
 
 private:
-    int bus_fd;             ///< I2C bus file descriptor.
+    using I2CDevID = std::tuple<std::string, std::uint16_t>;  ///< Tuple of device path and I2C address.
+
+    std::unique_lock<std::mutex> device_lock;
+    int bus_fd;  ///< I2C bus file descriptor.
     std::uint16_t address;  ///< I2C device address.
+
+    static std::unique_lock<std::mutex> lock_device(const I2CDevID& dev_id);
 };
 
 #endif  // FLIGHT_CONTROLLER_I2C_DEV_H
