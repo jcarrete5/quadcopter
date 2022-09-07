af4705091660e75973d1927a4a2a5a050988e7b7
Jason Carrete <jasoncarrete5@gmail.com>
2022-09-07 01:09:04 -0400

    Add pthread error check function

--- flight-controller/error.h
+++ flight-controller/error.h
@@ -7,7 +7,9 @@
 /**
  * @brief Check system call return value for errors.
  *
- * If the system call failed for any reason, throw an exception.
+ * If the system call failed for any reason, throw an exception. most system
+ * calls return -1 on failure in Linux. This function will check if the system
+ * call returned -1.
  *
  * @param return_value Value returned by the system call.
  * @returns The value returned by the system call.
@@ -19,6 +21,23 @@ inline auto check_syscall(std::signed_integral auto return_value)
         throw std::system_error(errno, std::system_category());
     }
     return return_value;
+}
+
+/**
+ * @brief Check pthread call return value for errors.
+ *
+ * pthread function calls return a positive error on failure and 0 on success.
+ *
+ * @param return_value The pthread function call return value.
+ * @return The pthread function call return value.
+ * @throw std::system_error Wrapped return value.
+ */
+inline auto check_pthread_call(std::signed_integral auto return_value)
+{
+    if (return_value != 0) {
+        throw std::system_error(return_value, std::system_category());
+    }
+   return return_value;
 }
 
 #endif  // FLIGHT_CONTROLLER_ERROR_H
