39f4675d5b69866a710fe6b32375f3d605808a3c
Jason Carrete <jasoncarrete5@gmail.com>
2022-09-09 13:01:27 -0400

    Start work for handling MPU6050 interrupts

--- flight-controller/main.cpp
+++ flight-controller/main.cpp
@@ -6,6 +6,10 @@
 int main()
 {
     MPU6050 mpu{MPU6050::Config{}};
-    std::cout << "WHO_AM_I -> " << std::showbase << std::setbase(16) << mpu.who_am_i() << '\n';
+    std::cerr << "WHO_AM_I -> " << std::showbase << std::hex << mpu.who_am_i() << '\n';
+
+    std::cerr << "main thread start sleeping\n";
+    std::this_thread::sleep_for(std::chrono::seconds{11});
+    std::cerr << "main thread done sleeping\n";
     return 0;
 }
