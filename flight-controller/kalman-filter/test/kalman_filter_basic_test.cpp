0d548a56942f681ef25471b6caab40375a28792f
Derek McBlane <mcblanederek@gmail.com>
2022-09-18 00:44:25 +0000

    kalman filter test setup

---
+++ flight-controller/kalman-filter/test/kalman_filter_basic_test.cpp
@@ -0,0 +1,14 @@
+#include "kalman_filter.h"
+
+#include "gtest/gtest.h"
+
+
+class RocketAltitudeFixture : public ::testing::Test {
+protected:
+};
+
+TEST(RocketAltitudeTest, Update) {
+}
+
+TEST(RocketAltitudeTest, Predict) {
+}
---
+++ flight-controller/kalman-filter/test/kalman_filter_hello_test.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(KalmanFilterTest, Hello) {
+    EXPECT_EQ(7*6, 42);
+}
