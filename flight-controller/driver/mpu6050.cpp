4cc549ec3fda5a0d27efad2f75c4473eb140edd8
Jason Carrete <jasoncarrete5@gmail.com>
2022-09-18 23:21:44 -0400

    Read data into circular buffer

--- flight-controller/driver/mpu6050.cpp
+++ flight-controller/driver/mpu6050.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <mutex>
 
 #include <gpiod.hpp>
 #include <iomanip>
@@ -115,13 +116,28 @@ enum class [[maybe_unused]] Register : std::uint8_t {
 MPU6050::MPU6050(const Config& config)
     : device{config.bus_path, config.i2c_address},
       event_loop_interrupted{false},
-      event_thread{[this, &config]() { event_loop(config); }}
+      event_thread{[this, config]() { event_loop(config); }},
+      data_buffer_mutex{},
+      data_in_buffer{},
+      data_buffer{config.data_buffer_capacity}
 {
     if (who_am_i() != MPU6050::i2c_address) {
         throw std::runtime_error{"I2C device is not an MPU6050"};
     }
 
     check_pthread_call(pthread_setname_np(event_thread.native_handle(), "MPU6050_event"));
+
+    reset();
+
+    // Configure full-scale ranges
+    device.write(static_cast<std::uint8_t>(Register::gyro_config), static_cast<std::uint8_t>(config.gyro_range) << 3);
+    device.write(static_cast<std::uint8_t>(Register::accel_config), static_cast<std::uint8_t>(config.accel_range) << 3);
+
+    // Enable DATA_READY interrupt
+    device.write(static_cast<std::uint8_t>(Register::int_enable), 0x01);
+
+    // Wake up device
+    device.write(static_cast<std::uint8_t>(Register::pwr_mgmt_1), 0x00);
 }
 
 MPU6050::~MPU6050()
@@ -201,14 +217,149 @@ void MPU6050::event_loop(const Config& config)
     );
 
     while (!event_loop_interrupted) {
+        // TODO: Maybe one day this can just be indefinite and the thread can be interrupted properly
         const bool timed_out{!line.event_wait(config.event_wait_timeout)};
         if (timed_out) {
             continue;
         }
 
-        gpiod::line_event ev{line.event_read()};
-        std::cerr << std::dec << "Event detected: [" << duration_cast<microseconds>(ev.timestamp).count()
-                  << "] " << ev.event_type << '\n';
-        // TODO Request sensor data from I2C bus, buffer it, and clear interrupt status
+        gpiod::line_event event{line.event_read()};
+        std::cerr << std::dec << "Event detected: [" << event.timestamp.count() << "] " << event.event_type << '\n';
+
+        // Read and clear interrupt status on MPU
+        std::uint8_t interrupt_status{device.read(static_cast<std::uint8_t>(Register::int_status))};
+        if (interrupt_status != 0x01) {
+            std::cerr << "Skipping non-data-ready interrupt: " << std::hex << std::showbase
+                      << static_cast<int>(interrupt_status) << '\n';
+            std::cerr << std::dec;
+            continue;
+        }
+
+        // Save sensor data
+        try {
+            std::vector<std::uint8_t> raw_data = device.read(static_cast<std::uint8_t>(Register::accel_xout_h), 14);
+            push_sample(
+                {
+                    event.timestamp,
+                    static_cast<std::int16_t>(raw_data[0] << 8 | raw_data[1])
+                        * gyro_full_scale_factor(config.gyro_range),
+                    static_cast<std::int16_t>(raw_data[2] << 8 | raw_data[3])
+                        * gyro_full_scale_factor(config.gyro_range),
+                    static_cast<std::int16_t>(raw_data[4] << 8 | raw_data[5])
+                        * gyro_full_scale_factor(config.gyro_range),
+                    static_cast<std::int16_t>(raw_data[6] << 8 | raw_data[7]) / 340.0 + 36.53,
+                    static_cast<std::int16_t>(raw_data[8] << 8 | raw_data[9])
+                        * accel_full_scale_factor(config.accel_range),
+                    static_cast<std::int16_t>(raw_data[10] << 8 | raw_data[11])
+                        * accel_full_scale_factor(config.accel_range),
+                    static_cast<std::int16_t>(raw_data[12] << 8 | raw_data[13])
+                        * accel_full_scale_factor(config.accel_range),
+                }
+            );
+        } catch (std::system_error& e) {
+            if (e.code().value() == 121) {
+                std::cerr << "Remote I/O error during MPU6050 event loop. Skipping failed read\n";
+            } else {
+                throw;
+            }
+        }
+    }
+}
+
+/**
+ * @brief Reset device registers and signal paths.
+ */
+void MPU6050::reset() const
+{
+    device.write(static_cast<std::uint8_t>(Register::pwr_mgmt_1), 0x80);
+    std::this_thread::sleep_for(std::chrono::milliseconds{100});
+    device.write(static_cast<std::uint8_t>(Register::signal_path_reset), 0x07);
+    std::this_thread::sleep_for(std::chrono::milliseconds{100});
+}
+
+/**
+ * @brief Push new sample data at the back of the buffer.
+ *
+ * The buffer mutex is locked before attempted to add a new sample. As a result,
+ * this function may block briefly if the lock cannot be acquired immediately.
+ *
+ * @param sample New sample to add.
+ */
+void MPU6050::push_sample(const MPU6050::Sample& sample)
+{
+    {
+        std::lock_guard buffer_guard{data_buffer_mutex};
+        data_buffer.push_back(sample);
+    }
+    data_in_buffer.notify_all();
+}
+
+/**
+ * @brief Remove the sample at the front of the buffer.
+ *
+ * This function will block if the buffer is empty or if the buffer mutex
+ * cannot be locked immediately.
+ *
+ * @return The removed Sample.
+ */
+MPU6050::Sample MPU6050::pop_sample()
+{
+    std::unique_lock buffer_guard{data_buffer_mutex};
+
+    // Wait until buffer has data
+    data_in_buffer.wait(buffer_guard, [this]() { return !data_buffer.empty(); });
+
+    Sample sample{data_buffer[0]};
+    data_buffer.pop_front();
+    return sample;
+}
+
+std::ostream& operator<<(std::ostream& os, const MPU6050::Sample& sample)
+{
+    os << '[' << sample.timestamp.count() << "] Accelerometer: (" << sample.accel_x << ", " << sample.accel_y << ", "
+       << sample.accel_z << ") Gyroscope: (" << sample.gyro_x << ", " << sample.gyro_y << ", " << sample.gyro_z
+       << ") Temperature: " << sample.temperature;
+    return os;
+}
+
+/**
+ * @param range The gyroscope full-scale range.
+ * @return The conversion factor used to convert raw gyroscope data to degrees per second.
+ */
+double MPU6050::gyro_full_scale_factor(MPU6050::GyroRange range)
+{
+    switch (range) {
+    case GyroRange::_250_degrees_per_second:
+        return 1.0 / 131.0;
+    case GyroRange::_500_degrees_per_second:
+        return 1.0 / 65.5;
+    case GyroRange::_1000_degrees_per_second:
+        return 1.0 / 32.8;
+    case GyroRange::_2000_degrees_per_second:
+        return 1.0 / 16.4;
+    default:
+        // Enumerator not implemented
+        assert(false);
+    }
+}
+
+/**
+ * @param range The accelerometer full-scale range.
+ * @return The conversion factor used to convert raw accelerometer data to g-force.
+ */
+double MPU6050::accel_full_scale_factor(MPU6050::AccelRange range)
+{
+    switch (range) {
+    case AccelRange::_2g:
+        return 1.0 / 16384.0;
+    case AccelRange::_4g:
+        return 1.0 / 8192.0;
+    case AccelRange::_8g:
+        return 1.0 / 4096.0;
+    case AccelRange::_16g:
+        return 1.0 / 2048.0;
+    default:
+        // Enumerator not implemented
+        assert(false);
     }
 }
--- flight-controller/main.cpp
+++ flight-controller/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include <iomanip>
 
 #include "driver/mpu6050.h"
 
 int main()
 {
     MPU6050 mpu{MPU6050::Config{}};
-    std::cerr << "WHO_AM_I -> " << std::showbase << std::hex << mpu.who_am_i() << '\n';
-
-    std::cerr << "main thread start sleeping\n";
-    std::this_thread::sleep_for(std::chrono::seconds{11});
-    std::cerr << "main thread done sleeping\n";
+    for (int i = 0; i < 1000; ++i) {
+        std::cout << mpu.pop_sample() << '\n';
+    }
     return 0;
 }
