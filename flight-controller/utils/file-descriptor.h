6caca726471cc05480adc4d0fe4c267b8e041897
Derek McBlane <mcblanederek@gmail.com>
2022-09-21 14:33:55 +0000

    minor cleanups: make snake_case class name CamelCase; put local headers last; fix acces modifiers for raw_descriptor_type and no_value

--- flight-controller/utils/file-descriptor.h
+++ flight-controller/utils/file-descriptor.h
@@ -8,23 +8,26 @@
  *
  * `open` is called on construction
  * `close` is called on destruction
+ * Allows for implicit conversion to `int`
  */
-class file_descriptor {
-    using file_descriptor_type = int;
-    static constexpr file_descriptor_type no_value = -1;
+class FileDescriptor {
 public:
-    file_descriptor(const std::string& path_name, int flags, mode_t mode = 0) noexcept;
-    ~file_descriptor() noexcept;
+    using raw_descriptor_type = int;
 
-    file_descriptor(const file_descriptor& other) = delete;
-    file_descriptor& operator=(const file_descriptor& other) = delete;
-    file_descriptor(file_descriptor&& other);
-    file_descriptor& operator=(file_descriptor&& other);
+    FileDescriptor(const std::string& path_name, int flags, mode_t mode = 0) noexcept;
+    ~FileDescriptor() noexcept;
 
-    operator file_descriptor_type() const;
+    FileDescriptor(const FileDescriptor& other) = delete;
+    FileDescriptor& operator=(const FileDescriptor& other) = delete;
+    FileDescriptor(FileDescriptor&& other);
+    FileDescriptor& operator=(FileDescriptor&& other);
+
+    operator raw_descriptor_type() const;
 
 private:
-    file_descriptor_type raw_descriptor_;
+    static constexpr raw_descriptor_type no_value = -1;
+
+    raw_descriptor_type raw_descriptor_;
 };
 
 #endif// UTIL_FILE_DESCRIPTOR_H
